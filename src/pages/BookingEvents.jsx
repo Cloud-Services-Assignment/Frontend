a80dc9e1fd75a1d62c6142c874164ae15e2f92a0
Jakob Hjalmarsson <jakob.hjalmarsson@learnet.se>
2025-06-08 21:57:17 +0200

    Styled the event booking page

--- src/pages/BookingEvents.jsx
+++ src/pages/BookingEvents.jsx
@@ -1,6 +1,25 @@
+import Header from "../components/Header"
+
 function BookingEvents() {
   return (
-    <div>BookingEvents</div>
+    <>
+      <title>Book Event | Ventixe</title>
+      <Header pageTitle={"Book Event"} />
+      <main className="surface event-booking">
+        <h2 className="senary-heading">Adventure Gear Show</h2>
+        <form action="" className="form surface light">
+          <div className="form-inner">
+            <label htmlFor="FullName">Full Name</label>
+            <input type="text" id="FullName" name="FullName" />
+          </div>
+          <div className="form-inner">
+            <label htmlFor="Quantity">Quantity</label>
+            <input type="number" id="Quantity" name="Quantity" min={1} max={10}/>
+          </div>
+          <button className="btn">Book</button>
+        </form>
+      </main>
+    </>
   )
 }
 
