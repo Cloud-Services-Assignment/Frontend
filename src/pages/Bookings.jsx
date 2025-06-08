f0e1adbb1112ded8683560b6166ddf35dc62e839
Jakob Hjalmarsson <jakob.hjalmarsson@learnet.se>
2025-06-08 19:22:18 +0200

    Styled the bookings page

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -5,7 +5,30 @@ function Bookings() {
     <>
       <title>Bookings | Ventixe</title>
       <Header pageTitle={"Bookings"} />
-      <main className="surface">Bookings</main>
+      <main className="surface">
+        <table className="bookings surface light">
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Name</th>
+              <th>Event</th>
+              <th>Ticket Price</th>
+              <th>Qty</th>
+              <th>Amount</th>
+            </tr>
+          </thead>
+          <tbody className="booking">
+            <tr>
+              <td>2029/02/15</td>
+              <td>Jackson Moore</td>
+              <td>Adventure Gear Show</td>
+              <td>$40</td>
+              <td>2</td>
+              <td>$80</td>
+            </tr>
+          </tbody>
+        </table>
+      </main>
     </>
   )
 }
